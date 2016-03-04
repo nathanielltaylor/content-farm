9430c82923c13b8cd2ff6f09ba8fc5651cd7a841
nathanielltaylor <ntaylor390@gmail.com>
2016-03-04 01:10:32 -0500

    titles isolated

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,8 +3,13 @@ require 'open-uri'
 class HomesController < ApplicationController
   def index
     page = Nokogiri::HTML(open("https://www.buzzfeed.com", :allow_redirections => :all))
-    titles = page.at('div.col1').inner_text.squish.gsub(/[\n\t]/, "").gsub("var load_ad = true && (AD_DESIGN == 'gpt'); if( typeof acl != 'undefined' ) load_ad &= !acl.user_can('edit_hidden_ads'); if (load_ad) { loadGPTAd({ wid: ", "")
+    titles = page.at('div.col1').inner_text.squish.gsub(/[\n\t]/, "")
+    titles = titles.gsub(/(([0-9]*|a few|an|a|a half) (minutes|hours|minute|hour) ago [0-9]* responses)|Promoted by BuzzFeed Staff Sponsored/, "BREAKPOINT")
+    title_array = titles.split("BREAKPOINT")
+    title_array.delete_if { |t| /var load_ad/ =~ t }
     binding.pry
+    #implement persistence of titles and generate chains based on random sampling
+    #later move all functionality of interacting with buzzfeed to background worker or rake task
     buzzfactory = MarkovChainGenerator.new(titles)
     buzzfactory.find_patterns
     @content = []
