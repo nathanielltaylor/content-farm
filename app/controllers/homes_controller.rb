08127b77315e574fe16764b08fdb5a90d09c6757
nathanielltaylor <ntaylor390@gmail.com>
2016-03-03 23:49:02 -0500

    ad js filter

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,7 +3,8 @@ require 'open-uri'
 class HomesController < ApplicationController
   def index
     page = Nokogiri::HTML(open("https://www.buzzfeed.com", :allow_redirections => :all))
-    titles = page.at('div.col1').inner_text.gsub(/[\n\t]/, "").squish
+    titles = page.at('div.col1').inner_text.squish.gsub(/[\n\t]/, "").gsub("var load_ad = true && (AD_DESIGN == 'gpt'); if( typeof acl != 'undefined' ) load_ad &= !acl.user_can('edit_hidden_ads'); if (load_ad) { loadGPTAd({ wid: ", "")
+    binding.pry
     buzzfactory = MarkovChainGenerator.new(titles)
     buzzfactory.find_patterns
     @content = []
